24e886e758bb16c2137647967a3a0a5c60638561
forexms78 <qudgh9709@gmail.com>
2020-06-01 20:54:33 +0900

    곱하기를 추가했습니다

--- fragment1/app/src/main/java/com/example/fragment1/minusfrag.java
+++ fragment1/app/src/main/java/com/example/fragment1/minusfrag.java
@@ -1,6 +1,47 @@
 package com.example.fragment1;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class minusfrag extends Fragment {
+
+    EditText num3, num4;
+    TextView res2;
+    Button min2;
+    View.OnClickListener cl;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root;
+        root = inflater.inflate(R.layout.minus,container,false);
+
+        num3 = (EditText)root.findViewById(R.id.number3);
+        num4 = (EditText)root.findViewById(R.id.number4);
+        res2 = (TextView)root.findViewById(R.id.result2);
+        min2 = (Button)root.findViewById(R.id.minu2);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a, b, c;
+                a = Integer.parseInt(num3.getText().toString());
+                b = Integer.parseInt(num4.getText().toString());
+                c = a - b;
+                res2.setText("합계 = " + c);
+            }
+        };
+        min2.setOnClickListener(cl);
+
+        return root;//super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
--- fragment1/app/src/main/java/com/example/fragment1/multfrag.java
+++ fragment1/app/src/main/java/com/example/fragment1/multfrag.java
@@ -1,5 +1,7 @@
 package com.example.fragment1;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,23 +14,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class plusfrag extends Fragment {
+public class multfrag extends Fragment {
 
     EditText num1, num2;
     TextView res1;
-    Button add;
+    Button mul;
     View.OnClickListener cl;
 
+    @SuppressLint("CutPasteId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root;
-        root = inflater.inflate(R.layout.plus,container,false);
-
-        num1 = (EditText)root.findViewById(R.id.number1);
-        num2 = (EditText)root.findViewById(R.id.number2);
-        res1 = (TextView)root.findViewById(R.id.result1);
-        add = (Button)root.findViewById(R.id.add);
+        root = inflater.inflate(R.layout.mult, container,false);
+        num1 = (EditText)root.findViewById(R.id.number5);
+        num2 = (EditText)root.findViewById(R.id.number6);
+        res1 = (TextView)root.findViewById(R.id.result3);
+        mul = (Button)root.findViewById(R.id.gob);
+        num1.setBackgroundColor(Color.YELLOW);
+        num2.setBackgroundColor(Color.YELLOW);
 
         cl = new View.OnClickListener() {
             @Override
@@ -36,11 +40,11 @@ public class plusfrag extends Fragment {
                 int a, b, c;
                 a = Integer.parseInt(num1.getText().toString());
                 b = Integer.parseInt(num2.getText().toString());
-                c = a + b;
-                res1.setText("곱하기 = " + c);
+                c = a * b;
+                res1.setText("합계 = " + c);
             }
         };
-        add.setOnClickListener(cl);
+        mul.setOnClickListener(cl);
 
         return root;//super.onCreateView(inflater, container, savedInstanceState);
     }
